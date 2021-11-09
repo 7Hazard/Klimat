e94a126e1b77f198813ae6a7d476267c94030b25
Hazard <hazard9909@gmail.com>
2021-11-09 16:17:09 +0100

    Initial Commit

---
+++ Klimat/KlimatApp.swift
@@ -0,0 +1,17 @@
+//
+//  KlimatApp.swift
+//  Klimat
+//
+//  Created by Leo Zaki on 2021-11-09.
+//
+
+import SwiftUI
+
+@main
+struct KlimatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
